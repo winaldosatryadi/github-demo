bc08533834fef1562b5dcc8f45b0ffccec72374c
winaldomanurung <winaldo.code@gmail.com>
2021-10-18 14:21:12 +0700

    done styling form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Date from "./components/Date";
+import ToDoForm from "./components/ToDoForm";
 import ToDoItem from "./components/ToDoItem";
 import ToDoList from "./components/ToDoList";
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       {/* <Date /> */}
       {/* <ToDoItem /> */}
+      <ToDoForm />
       <ToDoList />
     </div>
   );
